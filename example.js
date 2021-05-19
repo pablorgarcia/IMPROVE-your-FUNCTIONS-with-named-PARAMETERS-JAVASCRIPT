b1635996a439fa7c8d14f14a19dc0eb156fd6ae8
pablorgarcia <pablo.ramon.garcia.fernandez@gmail.com>
2021-05-19 11:36:59 +0200

    init

---
+++ example.js
@@ -0,0 +1,34 @@
+// funcion poco escalable
+function createGodZilla (color, weight = 200, sound, powers) {
+  // ...
+}
+
+createGodZilla('blue', undefined, ['lighting'])
+
+
+// podemos pasar un objeto como parametros
+function createGodZilla (
+  {
+    color, 
+    material = 'metal', 
+    weight= 200, 
+    sound, 
+    powers
+  }) {
+  // ...
+}
+
+/* 
+Con más de dos parametros es interesante pasarselos como un objeto 
+porque mantienes parametros por defecto sin necesidad de ingresarlos
+y porque puedes modificar los parametros de forma muy simple
+el mantenimiento también es mucho mejor
+*/
+const godzillaParams = {
+  color: 'blue',
+  material: 'plastic',
+  sound: 'GROAARRR',
+  powers: ['lighting', 'nuclear bomb']
+}
+
+createGodZilla(godzillaParams)
